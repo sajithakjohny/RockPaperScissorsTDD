bfb22f954ad64bb3f7e383a1ea316f5ecf98fb9d
K Johny, Sajitha <sajitha.k.johny@accenture.com>
2021-11-19 20:29:13 +0530

    First Commit for Program

---
+++ RockPaperScissorsDev/RockPaperScissorsGameDev.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissorsDev
+{
+   public  class RockPaperScissorsGameDev
+    {
+        /* ---------------------------------------------------------------
+        Rock beats Scissors, Scissors beats Paper, Paper beats Rock.
+        Your task is to create the core logic that checks which gesture wins in a matchup. For example:
+        Gesture 1	Gesture 2	Result
+        rock	scissors	rock beats scissors
+        paper	rock	paper beats rock
+        Paper  Scissors = Scissors wins
+        If both people choose the same thing, then it is a draw and nobody wins.
+       ---------------------------------------------------------------*/
+        public string DetermineWinner(string gesture1, string gesture2)
+        {
+            return null;
+        }
+    }
+}
---
+++ RockPaperScissorsTest/RockPaperScissorsGameTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RockPaperScissorsDev;
+using Xunit;
+
+namespace RockPaperScissorsTest
+{
+    public class RockPaperScissorsGameTest
+    {
+        RockPaperScissorsGameDev rockPaperScissorsGameDev = new RockPaperScissorsGameDev();
+        [Fact]
+        public void rockShouldCrushScissors()
+        {
+            //Arrange
+            string expectedResult = "rock beats scissors";
+            //Act
+            string result = rockPaperScissorsGameDev.DetermineWinner("rock", "scissors");
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
